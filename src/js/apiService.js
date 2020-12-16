5fe26edfd5e2b111baa37501244d6aa122f35317
Vova Rabeshko <rabeshko23@gmail.com>
2020-12-16 18:14:13 +0200

    скролл редаг

---
+++ src/js/apiService.js
@@ -0,0 +1,47 @@
+import { myPnotify } from './pnotify';
+
+const API_KEY = '19539896-abe26f00558e3a292a16c6b93';
+const BASE_URL = 'https://pixabay.com/api/'
+
+export default class PixabayApiServise {
+    constructor() {
+        this.searchQuery = '';
+        this.page = 1;
+    }
+
+    async fetchImages() {
+        try {
+            const url = `${BASE_URL}?image_type=photo&orientation=horizontal&q=${this.searchQuery}&page=${this.page}&per_page=12&key=${API_KEY}`;
+            const resultFetch = await fetch(url).then(res => res.json());
+
+            this.incrementPage();
+
+            if (resultFetch.total === 0) {
+                myPnotify('Not Found images for your request.Please try again');
+                return [];
+            }
+
+            return resultFetch;
+        } catch (error) {
+            myPnotify('Imges is ended');
+            console.log(error);
+            return error;
+        }
+    }
+
+    incrementPage() {
+        this.page += 1;
+    }
+
+    resetPage() {
+        this.page = 1;
+    }
+
+    get query() {
+        return this.searchQuery;
+    }
+
+    set query(newQuery) {
+        this.searchQuery = newQuery;
+    }
+}
---
+++ src/js/refs.js
@@ -0,0 +1,5 @@
+export const refs = {
+    input: document.querySelector('.search-form'),
+    outputList: document.querySelector('.gallery'),
+    watcher: document.querySelector('.watcher'),
+}
